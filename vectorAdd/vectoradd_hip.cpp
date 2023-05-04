7446ce50e6747a091119adedc0f3a6cea8615c92
Wen-Heng (Jack) Chung <whchung@gmail.com>
2023-05-04 15:11:19 -0500

    test commit

--- vectorAdd/vectoradd_hip.cpp
+++ vectorAdd/vectoradd_hip.cpp
@@ -45,6 +45,26 @@ THE SOFTWARE.
 
 #define TEST_111 (0)
 
+#define GET_WRITE_READ_DISPATCH_ID \
+        ((uint32_t __attribute__((address_space(4)))*)__builtin_amdgcn_queue_ptr())[ \
+           10 \
+          + 4 \
+        ] << 16 \
+        | \
+        ((uint32_t __attribute__((address_space(4)))*)__builtin_amdgcn_queue_ptr())[ \
+           10 \
+          + 4 \
+          + 2 \
+          + 1 \
+          + 1 \
+          + 1 \
+          + 1 \
+          + 2 \
+          + 1 \
+          + 9 \
+        ];
+ 
+
 __global__ void 
 vectoradd_float(const float* __restrict__ a, const float* __restrict__ b, float* __restrict__ c)
 
@@ -55,13 +75,88 @@ vectoradd_float(const float* __restrict__ a, const float* __restrict__ b, float*
       reinterpret_cast<unsigned int*>(c)[2] = hipBlockDim_x;
       reinterpret_cast<unsigned int*>(c)[3] = hipBlockDim_x;
 #else
+      // fetch read_dispatch_id and write_dispatch_id
+      uint32_t v = GET_WRITE_READ_DISPATCH_ID;
 
       int x = THREADS_PER_BLOCK_X * hipBlockIdx_x + hipThreadIdx_x;
       int y = THREADS_PER_BLOCK_Y * hipBlockIdx_y + hipThreadIdx_y;
 
       int i = y * WIDTH + x;
-      c[i] = a[i] + b[i];
+
+      // obtain barrier bit in AQL packet
+      int barrier_bit = 
+        ((uint32_t __attribute__((address_space(4)))*)__builtin_amdgcn_dispatch_ptr())[
+        0
+        ] >> 8 & 0x1;
+
+      if (!barrier_bit) {
+        //reinterpret_cast<uint32_t*>(c)[i] = GET_WRITE_READ_DISPATCH_ID;
+
+#if 0
+        ((uint32_t __attribute__((address_space(4)))*)__builtin_amdgcn_queue_ptr())[
+           10   // hsa_quque
+          + 4   // reserved1
+          //+ 2   // write_dispatch_id
+          //+ 1   // group_segment_aperture_base_hi
+          //+ 1   // private_segment_aperture_base_hi
+          //+ 1   // max_cu_id
+          //+ 1   // max_wave_id
+          //+ 2   // max_legacy_doorbell_dispatch_id_plus_1
+          //+ 1   // legacy_doorbell_lock
+          //+ 9   // reserved2
+
+          //+ 2   // read_dispatch_id
+          //+ 1   // read_dispatch_id_field_base_byte_offset
+          //+ 1   // compute_tmpring_size
+          //+ 4   // scratch_resource_descriptor
+          //+ 2   // scratch_backing_memory_location
+          //+ 2   // scratch_backing_memory_byte_size
+          //+ 1   // scratch_wave64_lane_byte_size
+          //+ 1   // queue_properties
+          //+ 2   // reserved3
+          //+ 2   // queue_inactive_signal
+        ] << 16
+
+        |
+
+        ((uint32_t __attribute__((address_space(4)))*)__builtin_amdgcn_queue_ptr())[
+           10   // hsa_quque
+          + 4   // reserved1
+          + 2   // write_dispatch_id
+          + 1   // group_segment_aperture_base_hi
+          + 1   // private_segment_aperture_base_hi
+          + 1   // max_cu_id
+          + 1   // max_wave_id
+          + 2   // max_legacy_doorbell_dispatch_id_plus_1
+          + 1   // legacy_doorbell_lock
+          + 9   // reserved2
+
+          //+ 2   // read_dispatch_id
+          //+ 1   // read_dispatch_id_field_base_byte_offset
+          //+ 1   // compute_tmpring_size
+          //+ 4   // scratch_resource_descriptor
+          //+ 2   // scratch_backing_memory_location
+          //+ 2   // scratch_backing_memory_byte_size
+          //+ 1   // scratch_wave64_lane_byte_size
+          //+ 1   // queue_properties
+          //+ 2   // reserved3
+          //+ 2   // queue_inactive_signal
+        ];
 #endif
+        if (v != 0x0041003F) {
+          reinterpret_cast<uint32_t*>(c)[i] = v;
+        } else {
+          __builtin_amdgcn_s_sleep(127);
+          reinterpret_cast<uint32_t*>(c)[i] = GET_WRITE_READ_DISPATCH_ID;
+        }
+
+      } else {
+        c[i] = a[i] + b[i];
+      }
+#endif
+
+      for (int i = 0; i < 10000; ++i)
+        reinterpret_cast<uint32_t*>(c)[i] = v;
   }
 
 #if 0
@@ -128,17 +223,21 @@ int main() {
                   0, 0,
                   deviceA ,deviceB ,deviceC ,WIDTH ,HEIGHT);
 #else
+  for (int i = 0; i < 256; ++i) {
   hipLaunchKernelGGL(vectoradd_float,
                   dim3(WIDTH/THREADS_PER_BLOCK_X, HEIGHT/THREADS_PER_BLOCK_Y),
                   dim3(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y),
                   0, 0,
                   deviceA ,deviceB ,deviceC);
                   //deviceA ,deviceB ,deviceC ,WIDTH ,HEIGHT);
+  }
+  HIP_ASSERT(hipMemcpy(hostC, deviceC, NUM*sizeof(float), hipMemcpyDeviceToHost));
+  printf("c[0]: %08X\n", reinterpret_cast<unsigned int*>(hostC)[0]);
 #endif
 
 
-  HIP_ASSERT(hipMemcpy(hostC, deviceC, NUM*sizeof(float), hipMemcpyDeviceToHost));
 
+#if 0
 #if TEST_111
   printf("c[0]: %08X\n", reinterpret_cast<unsigned int*>(hostC)[0]);
   printf("c[1]: %08X\n", reinterpret_cast<unsigned int*>(hostC)[1]);
@@ -157,6 +256,7 @@ int main() {
   } else {
       printf ("PASSED!\n");
   }
+#endif
 #endif
 
   HIP_ASSERT(hipFree(deviceA));
