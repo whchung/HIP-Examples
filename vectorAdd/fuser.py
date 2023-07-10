53264ab3e2485740dbfeb0cf5f192892c04e3de5
Wen-Heng (Jack) Chung <whchung@gmail.com>
2023-07-10 11:57:13 -0500

    Initial commit of a python-based kernel fuser.
    This is a toy yet.

---
+++ vectorAdd/fuser.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python3
+
+import re
+
+# Configuration
+INPUT_FILE = "vectoradd_hip-hip-amdgcn-amd-amdhsa-gfx900.s"
+OUTPUT_FILE = "output.s"
+HOST_KERNEL = "_Z15vectoradd_floatPKfS0_PfS1_"
+GUEST_KERNEL = "_Z16vectoradd_float2PKfS0_S0_Pf"
+
+TEMPLATE_FILE_BEGIN = 'template_begin.s'
+TEMPLATE_FILE_END = 'template_end.s'
+
+# Constants
+MAYBE_EMPTY_SPACES = r'[ \t]*'
+NON_EMPTY_SPACES = r'[ \t]+'
+KERNEL_METADATA_DIRECTIVE = r'.amdhsa_kernel'
+KERNEL_METADATA_END_DIRECTIVE = r'.end_amdhsa_kernel'
+
+USER_SGPR_DIRECTIVES = [ r'amdhsa_user_sgpr_private_segment_buffer', \
+                         r'amdhsa_user_sgpr_dispatch_ptr', \
+                         r'amdhsa_user_sgpr_queue_ptr', \
+                         r'amdhsa_user_sgpr_kernarg_segment_ptr', \
+                         r'amdhsa_user_sgpr_dispatch_id', \
+                         r'amdhsa_user_sgpr_flat_scratch_init']
+USER_SGPR_CONSUMPTION = { r'amdhsa_user_sgpr_private_segment_buffer' : 4, \
+                          r'amdhsa_user_sgpr_dispatch_ptr' : 2, \
+                          r'amdhsa_user_sgpr_queue_ptr' : 2, \
+                          r'amdhsa_user_sgpr_kernarg_segment_ptr' : 2, \
+                          r'amdhsa_user_sgpr_dispatch_id' : 2, \
+                          r'amdhsa_user_sgpr_flat_scratch_init': 2 }
+
+NEXT_FREE_SGPR = r'amdhsa_next_free_sgpr'
+NEXT_FREE_VGPR = r'amdhsa_next_free_vgpr'
+
+SYSTEM_SGPR_WORKGROUP_ID = r'amdhsa_system_sgpr_workgroup_id_'
+
+DIMENSIONS = [ 'x', 'y', 'z' ]
+
+# Regex
+KERNEL_NAME_REGEX = MAYBE_EMPTY_SPACES + \
+                    "(" + KERNEL_METADATA_DIRECTIVE +")" + \
+                    NON_EMPTY_SPACES + \
+                    "(" + r'\w+' +")"
+
+KERNEL_CODE_BEGIN_REGEX = r':[ \t]+; @'
+KERNEL_CODE_END_REGEX = r'^[ \t]+\.section[ \t]+'
+
+KERNEL_METADATA_BEGIN_REGEX = MAYBE_EMPTY_SPACES + KERNEL_METADATA_DIRECTIVE + NON_EMPTY_SPACES
+KERNEL_METADATA_END_REGEX = MAYBE_EMPTY_SPACES + KERNEL_METADATA_END_DIRECTIVE
+KERNEL_METADATA_ENTRY_REGEX = MAYBE_EMPTY_SPACES + r'\.(\w+)' + NON_EMPTY_SPACES + r'(\d)' 
+
+# Parse input file, retrieve kernel names
+def retrieve_kernel_names(input_stream, output_list):
+  for input_line in input_stream:
+    m = re.search(KERNEL_NAME_REGEX, input_line)
+    if m is not None:
+      output_list.append(m.group(2))
+
+# Parse input stream, retrieve kernel source code
+def retrieve_kernel_source_code(kernel_name, input_stream, output_dict):
+  inside_kernel_code = False
+  for input_line in input_stream:
+    if inside_kernel_code is False:
+      m = re.search(kernel_name + KERNEL_CODE_BEGIN_REGEX + kernel_name, input_line)
+      if m is not None:
+        # Found beginning of code
+        output_dict[kernel_name] = []
+        # XXX Ignore the very first line
+        #output_dict[kernel_name].append(input_line)
+        inside_kernel_code = True
+        continue
+    else:
+      m = re.search(KERNEL_CODE_END_REGEX, input_line)
+      if m is not None:
+        # Found end of code
+        inside_kernel_code = False
+        break
+  
+      output_dict[kernel_name].append(input_line.rstrip())
+
+# Parse input stream, retrieve kernel metadata
+def retrieve_kernel_metadata(kernel_name, input_stream, output_dict):
+  inside_kernel_metadata = False
+  for input_line in input_stream:
+    if inside_kernel_metadata is False:
+      m = re.search(KERNEL_METADATA_BEGIN_REGEX + kernel_name, input_line)
+      if m is not None:
+        # Found beginning of metadata
+        output_dict[kernel_name] = {}
+        inside_kernel_metadata = True
+        continue
+    else:
+      m = re.search(KERNEL_METADATA_END_REGEX, input_line)
+      if m is not None:
+        # Found end of metadata
+        inside_kernel_metadata = False
+        break
+
+      m = re.search(KERNEL_METADATA_ENTRY_REGEX, input_line)
+      if m is not None:
+        # Found metadata
+        if len(m.groups()) == 2:
+          output_dict[kernel_name][m.group(1)] = int(m.group(2))
+
+
+# Lists
+kernel_name_list = []
+
+# Dicts
+kernel_code_dict = {}
+kernel_metadata_dict = {}
+
+# Open input file
+input_file = open(INPUT_FILE, "r")
+
+retrieve_kernel_names(input_file, kernel_name_list)
+
+# # Identify host and guest kernel
+# for kernel_name in kernel_name_list:
+#   if kernel_name == HOST_KERNEL:
+#     print("Host kernel found: " + kernel_name)
+#   elif kernel_name == GUEST_KERNEL:
+#     print("Guest kernel found: " + kernel_name)
+
+# Get kernel code and metadata
+input_file.seek(0)
+retrieve_kernel_source_code(HOST_KERNEL, input_file, kernel_code_dict)
+input_file.seek(0)
+retrieve_kernel_metadata(HOST_KERNEL, input_file, kernel_metadata_dict)
+
+input_file.seek(0)
+retrieve_kernel_source_code(GUEST_KERNEL, input_file, kernel_code_dict)
+input_file.seek(0)
+retrieve_kernel_metadata(GUEST_KERNEL, input_file, kernel_metadata_dict)
+
+
+# Understand context to be saved:
+metadata_dict = kernel_metadata_dict
+kernel_name = GUEST_KERNEL
+
+# user sgprs set by CP
+user_sgpr_cp_count = 0
+# Count user sgprs be used
+for user_sgpr in USER_SGPR_DIRECTIVES:
+  if metadata_dict[kernel_name][user_sgpr] == 1:
+    user_sgpr_cp_count += USER_SGPR_CONSUMPTION[user_sgpr]
+
+#print("User sgpr set by CP: " + str(user_sgpr_cp_count))
+
+# user sgprs set by ADC
+# workgroup ID + workitem ID
+user_sgpr_adc_count = 0
+for dim in DIMENSIONS:
+  if kernel_metadata_dict[GUEST_KERNEL][SYSTEM_SGPR_WORKGROUP_ID + dim] == 1:
+    user_sgpr_adc_count += 1
+
+#print("User sgpr set by ADC: " + str(user_sgpr_adc_count))
+
+# find next free SGPR / VGPR
+# TBD: need to align between GUEST and HOST
+next_free_sgpr = kernel_metadata_dict[GUEST_KERNEL][NEXT_FREE_SGPR]
+next_free_vgpr = kernel_metadata_dict[GUEST_KERNEL][NEXT_FREE_VGPR]
+
+#print("Next free SGPR: " + str(next_free_sgpr))
+#print("Next free VGPR: " + str(next_free_vgpr))
+
+# Produce context save/restore logic
+# TBD: consider SGPRs used by CP, align between GUEST and HOST
+
+context_save_logic = []
+context_save_logic.append("\t; save context")
+context_restore_logic = []
+context_restore_logic.append("\t; restore context")
+
+next_sgpr = next_free_sgpr
+next_vgpr = next_free_vgpr
+user_sgpr_adc_saved = 0
+for d in range(len(DIMENSIONS)):
+  dim = DIMENSIONS[d]
+  if kernel_metadata_dict[GUEST_KERNEL][SYSTEM_SGPR_WORKGROUP_ID + dim] == 1:
+    context_save_logic.append('\ts_mov_b32_e32' + ' ' + 's' + str(next_sgpr) + ', ' + 's' + str(user_sgpr_cp_count + user_sgpr_adc_saved))
+    context_save_logic.append('\tv_mov_b32_e32' + ' ' + 'v' + str(next_vgpr) + ', ' + 'v' + str(d))
+
+    context_restore_logic.append('\ts_mov_b32_e32' + ' ' + 's' + str(user_sgpr_cp_count + user_sgpr_adc_saved) + ', ' + 's' + str(next_sgpr))
+    context_restore_logic.append('\tv_mov_b32_e32' + ' ' + 'v' + str(d) + ', ' + 'v' + str(next_vgpr))
+
+    next_sgpr += 1
+    next_vgpr += 1
+    user_sgpr_adc_saved += 1
+
+# Manipulate host kernel, disable s_endpgm
+for line_number in range(len(kernel_code_dict[HOST_KERNEL])):
+  line = kernel_code_dict[HOST_KERNEL][line_number]
+  m = re.search(r'^[ \t]+s_endpgm', line)
+  if m is not None:
+    kernel_code_dict[HOST_KERNEL][line_number] = "\t;s_endpgm"
+
+# Manipulate guest kernel, rename BBs
+for line_number in range(len(kernel_code_dict[GUEST_KERNEL])):
+  line = kernel_code_dict[GUEST_KERNEL][line_number]
+  m = re.search(r'(.*)BB(\d+)_(\d+)(.*)', line)
+  if m is not None:
+    kernel_code_dict[GUEST_KERNEL][line_number] = m.group(1) + r'FUSED_BB' + m.group(2) + '_' + m.group(3) + m.group(4)
+
+# Start fusion
+kernel_code_dict[HOST_KERNEL] = context_save_logic + kernel_code_dict[HOST_KERNEL] + context_restore_logic + kernel_code_dict[GUEST_KERNEL]
+
+template_begin_file = open(TEMPLATE_FILE_BEGIN, 'r')
+for line in template_begin_file:
+  print(line.rstrip())
+
+for line in kernel_code_dict[HOST_KERNEL]:
+  print(line)
+
+template_end_file = open(TEMPLATE_FILE_END, 'r')
+for line in template_end_file:
+  print(line.rstrip())
+
